8eecf03ebbaef444afe2a8d5b0b2e70e247e8131
ventocilla.leo@gmail.com <@24MaluOK@>
2020-12-16 16:14:23 -0300

    added cidades.json

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
+/*
+combos: ^1.2.0
+mobx: ^1.1.1
+flutter_mobx: ^1.1.1
+
+dev-depen:
+build_runner: ^1.8.1
+mobx_codegen: ^1.0.3
+
+ibge cidade api
+
+*/
 void main() {
   runApp(MyApp());
 }
