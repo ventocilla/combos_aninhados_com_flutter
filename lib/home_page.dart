b35ac17972193a1f0cfb3c588375d5589d0d8091
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2020-12-17 20:31:23 -0300

    updated HomePage with ListCombo & pubspec.yaml

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:combos/combos.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,10 +10,19 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Container(),
-        ],
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ListCombo(
+              child: Text('Selecione um Estado'),
+              getList: () {  }, onItemTapped: (value) {  }, itemBuilder: (BuildContext context, ComboParameters parameters, item) {  },),
+            ListCombo(
+              child: Text('Selecione uma Cidade'),
+              getList: () {  }, onItemTapped: (value) {  }, itemBuilder: (BuildContext context, ComboParameters parameters, item) {  },),
+          ],
+        ),
       ),
     );
   }
