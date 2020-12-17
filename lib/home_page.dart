9581a81342876cd6ec5751beab80a0cd2bc74d14
ventocilla.leo@gmail.com <@24MaluOK@>
2020-12-16 23:48:33 -0300

    updated HomePage.dart and main.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,6 +8,12 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Container(),
+        ],
+      ),
+    );
   }
 }
